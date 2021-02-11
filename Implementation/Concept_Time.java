073a42ed582838dad51fc68fca40585319525c02
eful5678 <77581778+eful5678@users.noreply.github.com>
2021-02-11 21:21:18 +0900

    hello

--- Implementation/Concept_Time.java
+++ Implementation/Concept_Time.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Concept_Time {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("첫번째 정수 a를 입력해주세요 : ");
+        int a = sc.nextInt();
+
+        System.out.println("두번째 정수 b를 입력해주세요 : ");
+        int b = sc.nextInt();
+
+        System.out.println("입력 값 a : " + a);
+        System.out.println("입력 값 b : " + b);
     }
 }
---
+++ Implementation/Concepts.md
@@ -0,0 +1,6 @@
+# Implementation
+
+## What is Implementation?
+
+    - 머릿속에 있는 알고리즘을 소스코드로 바꾸는 과정
+        - Problem -> Thinking -> Solution
