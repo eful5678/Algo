f95627e9ede35dd8265f6c626dd8923f2251f24f
eful5678 <77581778+eful5678@users.noreply.github.com>
2021-02-15 16:18:58 +0900

    finish 1 algorithm

---
+++ temp/src/Back1978_PrimeNumber.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Back1978_PrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner(System.in);
+        // 숫자 입력 횟수
+        int input = sc.nextInt();
+        
+        int temp = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < input; i++){
+            int p = sc.nextInt();
+            for(int j = 1; j < 1001; j++){
+                if(p % j == 0){
+                    temp++;
+                }
+            }
+            if(temp == 2){
+                cnt++;
+            }
+            temp = 0;
+        }
+        System.out.println(cnt);
+    }
+}
