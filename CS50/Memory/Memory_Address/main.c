7972ebb367f30dd34495d8a8b2846bb8457b1b5b
eful5678 <77581778+eful5678@users.noreply.github.com>
2021-02-23 00:34:59 +0900

    String Pointer

--- CS50/Memory/Memory_Address/main.c
+++ CS50/Memory/Memory_Address/main.c
@@ -7,6 +7,17 @@ int main(void)
     printf("%p\n", &n); // & -> address is~
 
     printf("%i\n", *&n);// opposite of & : * -> go to address
+
+    int *p = &n; // * -> store n's address in p -> p is pointer
+    printf("%p\n", p); // return address
+    printf("%i\n", *p); // return value
+
+    char *s = "EMMA";
+
+
+    printf("%p\n", s);
+    printf("%p\n", &s[0]);
+
 }
 
 // n's address : 0x7ffca6451acc -> pointer value
