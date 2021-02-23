92fe70276216e9433e711ed372f7339d558907c9
eful5678 <77581778+eful5678@users.noreply.github.com>
2021-02-24 00:11:11 +0900

    Josephus1

---
+++ DataStructure/src/Baek1158_JosephusProblem.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Baek1158_JosephusProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+
+        int[] people = new int[N];
+    }
+}
